7f3d291aae32b8f8910d6c36dfe4f16855a88624
albertassi88 <albertassi88@gmail.com>
2021-02-14 17:05:21 -0300

    Alteracoes

--- 01-exemplo-redux/src/redux/reducers/account.js
+++ 01-exemplo-redux/src/redux/reducers/account.js
@@ -4,12 +4,8 @@ const INITIAL_STATE = { money: 10000 }
 
 const account = (state=INITIAL_STATE, action) => {
     switch (action.type) {
-        case SPEND_MONEY    :
-            
-            return {
-                money: state.money - action.value,
-            }
-    
+        case SPEND_MONEY:
+            return { money: state.money - action.value } 
         default:
             return state;
     }
